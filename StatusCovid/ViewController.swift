7045af1e6eeee3a6b9c7eb194fb0c81926929af2
Blanca Cordova <blancacordova@MacBook-Pro-de-Blanca.local>
2020-12-18 22:10:44 -0600

    Interfaz y Constrains

--- StatusCovid/ViewController.swift
+++ StatusCovid/ViewController.swift
@@ -10,11 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var paisLabel: UILabel!
+    
+    @IBOutlet weak var cLabel: UILabel!
+    @IBOutlet weak var mLabel: UILabel!
+    @IBOutlet weak var rLabel: UILabel!
+    
+    @IBOutlet weak var ctLabel: UILabel!
+    @IBOutlet weak var mtLabel: UILabel!
+    @IBOutlet weak var rtLabel: UILabel!
+    
+    @IBOutlet weak var casosLabel: UILabel!
+    @IBOutlet weak var muertesLabel: UILabel!
+    @IBOutlet weak var recuperadosLabel: UILabel!
+    
+    @IBOutlet weak var casostLabel: UILabel!
+    @IBOutlet weak var muertestLabel: UILabel!
+    @IBOutlet weak var recuperadostLabel: UILabel!
+    
+    @IBOutlet weak var imgBandera: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        LimpiarInfo(paisText: "Busca el pais",img: "covid3.png")
+
     }
-
-
+    func LimpiarInfo(paisText:String, img: String) {
+        paisLabel.text = paisText
+        cLabel.text = ""
+        mLabel.text = ""
+        rLabel.text = ""
+        
+        ctLabel.text = ""
+        mtLabel.text = ""
+        rtLabel.text = ""
+        
+        casosLabel.text = ""
+        muertesLabel.text = ""
+        recuperadosLabel.text = ""
+        
+        casostLabel.text = ""
+        muertestLabel.text = ""
+        recuperadostLabel.text = ""
+        
+        imgBandera.image = UIImage(named: img)
+    }
+    
 }
-
